a6fa3e5605bb12b2c85303886ca8b0160eaf497d
Revi <93813059+raflydtya@users.noreply.github.com>
2022-06-01 11:01:42 +0700

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -81,36 +81,7 @@ HELP_2 = """
 
 
 HELP_3 = """
-**Panduan Dasar**
-
-• Tambahkan bot ini ke grup atau channel dan berikan izin admin.
-• Cek menu `/help` untuk mendapatkan informasi perintah.
-• Jika akun asisten tidak bergabung otomatis silahkan hubungi @justcontactbot.
-• Jika akun asisten tidak bergabung di Voicechat, lakukan `/restart` untuk me-refresh.
-
-• Kamu dapat memutar musik dalam dua **mode pencarian** i.e. Mode Langsung dan Mode Sebaris.\n\nUbah mode melalui /playmode"
-• Kamu dapat memutar musik dalam dua **jenis bermain** i.e. Mode Semua Orang dan Mode Admin.\n\nUbah mode melalui /playmode"
-• Kamu Anda dapat memutar musik di **saluran** juga.\n\nMengatur channel_id melalui /channelplay dan bermain melalui /cplay "
-• Non Admin juga dapat menggunakan perintah admin dengan menambahkannya ke** DAFTAR PENGGUNA AUTH**. \nTambahkan pengguna mana pun ke daftar autentikasi dengan /auth , hapus dengan /unauth dan periksa pengguna autentik melalui /authusers"
-• Bot memiliki fitur yang disebut **Modus Bersih**.\nIni menghapus pesan bot setelah 5 Menit dan memastikan obrolan Anda tetap bersih.\n\nEnable atau disable mode bersih dari /settings [__Enabled by default__]"
-• Kamu bisa bermain **Spotify** trek dan daftar putar juga.\n\nMulai bermain sekarang dengan /play [Tautan Spotify]"
-• Kamu bisa bermain **Apple Musik** trek dan daftar putar juga.\n\nMulai bermain sekarang dengan /play [Tautan Apple]"
-• Kamu bisa bermain **Musik Resso** trek dan daftar putar juga.\n\nMulai bermain sekarang dengan /play [Tautan Resso]"
-• Kamu bisa bermain **Sound Cloud** trek dan daftar putar juga.\n\nMulai bermain sekarang dengan /play [Tautan SoundCloud]"
-• Kamu bisa bermain **Videos** dalam obrolan suara melalui /vplay [Nama Video] atau /play -v [Nama Video]"
-• Anda dapat mengatur **Kualitas Audio** obrolan suara ke Rendah, Sedang atau Tinggi.\n\nTetapkan kualitas melalui /settings"
-• Anda dapat mengatur **Kualitas video** obrolan suara ke Rendah, Sedang atau Tinggi.\n\nTetapkan kualitas melalui /settings"
-• Anda dapat memeriksa **Statistik** di bot seperti Top 10 Lagu yang Dimainkan.\n\nDapatkan Statistik: /gstats "
-• Anda dapat memeriksa **Statistik Grup** di bot seperti Top 10 Lagu yang Dimainkan.\n\nDapatkan Statistik: /gstats "
-• Anda sekarang dapat membisukan musik yang diputar di obrolan suara.\n\nMemerintah: /mute"
-• Kamu bisa membisukan dan membunyikan streaming. Gunakan perintah `/mute` atau `/unmute`
-• Kamu bisa mencari lirik musik. Gunakan perintah `/lyrics` - Judul.
-• Kamu bisa mengunduh musik di YouTube Server. Gunakan perintah `/song` - Judul atau tautan.
-• Bot menyimpan semua track yang pernah diputar di semua grup pengguna. Gunakan perintah `/play`.
-• Kamu bisa mengacak antrian yang berlangsung. Gunakan perintah `/shuffle`.
-• Kamu bisa melihat antrian yang sedang berlangsung. Gunakan perintah `/queue`.
-• Kamu bisa merubah bahasa sesuai selera. Gunakan perintah `/lang`.
-• Kamu bisa melakukan perintah stream secara paksa, itu akan menghentikan track yang berjalan sementara. Gunakan perintah `/playforce` - Judul.
+Under Maintenance
 """
 
 HELP_4 = """
