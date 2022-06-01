09028d857d55856a3da45d430f34b10a2664670a
Revi <93813059+raflydtya@users.noreply.github.com>
2022-06-01 11:08:54 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -32,7 +32,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup(_, videoid, result):
+def stream_markup(_, videoid):
     buttons = [
         [
             InlineKeyboardButton(
